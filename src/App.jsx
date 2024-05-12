e7126ccab9bf6a00035e299d42f957ae5793be90
Janhavi Chauhan <143059176+janhavichauhan@users.noreply.github.com>
2024-05-12 21:51:19 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-center text-3xl text-slate-600 h-screen'>Thank you </p>
+    </>
+  )
+}
+
+export default App
